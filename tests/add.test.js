43c6c42a487253e7eadd074c540242ffb3d987ac
Tran Long Chau <chau.tran@tuni.fi>
2020-11-22 12:08:08 +0200

    Add test cases: Add.js

---
+++ tests/add.test.js
@@ -0,0 +1,37 @@
+import add from '../src/add.js'
+
+describe('Basic addition', () => {
+    test('1: Addtion between integers', () => {
+        const result = add(1,2)
+        expect(result).toBe(3)
+    });
+
+    test('2: Addition between very large integers', () => {
+        const result = add(1000000000000000,2000000000000000);
+        expect(result).toBe(3000000000000000);
+    });
+    
+    test('3: Addtion between floats', () => {
+        const result = add(1.21354,27646.54)
+        expect(result).toBe(27647.75354)
+    });
+    
+    test('4: Addtion between signed numbers', () => {
+        const result = add(-464911.546,15412.87451)
+        expect(result).toBe(-449498.67149)
+    });    
+})
+
+describe('Undefined values', () => {
+    // This case means that both should be null for a default value? 
+    // By design or an overlook
+    test('1: Missing one or both params', () => {
+        const result = add(1);
+        expect(result).toBe(1);
+    });
+
+    test('2: Addition between very large integers', () => {
+        const result = add();
+        expect(result).toBe(0);
+    })    
+}) 
--- tests/divide.test.js
+++ tests/divide.test.js
@@ -5,5 +5,11 @@ describe('CamelCase', () => {
     test('1: divide returns correct answer', () => {
         const result = divide(1,2)
         expect(result).toBe(1/2)
+
+        result = divide(6,4)
+        expect(result).toBe(6/4)
+        
+        result = divide(9,3)
+        expect(result).toBe(3)        
     })
 }) 
