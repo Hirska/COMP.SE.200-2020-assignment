2e80b43210796ac41ffdb7884c3a6e7126e4b770
Aleksi Hirvonen <aleksi.hirvonen@tuni.fi>
2020-11-28 16:15:08 +0200

    Add tests for upperFirst

--- tests/upperFirst.test.js
+++ tests/upperFirst.test.js
@@ -1,15 +1,20 @@
 import upperFirst from '../src/upperFirst.js'
 
 describe('upperFirst', () => {
-    it('should stay same if all caps is given', () => {
+    it('1: should stay same if all caps is given', () => {
         const result = upperFirst('TEST')
         expect(result).toBe('TEST')
     })
-    it('should uppercase first if lowercase is given', () => {
+    it('2: should uppercase first if lowercase is given', () => {
         const result = upperFirst('tEST')
         expect(result).toBe('TEST')
     })
-    it('should not do anything if first is not char', () => {
+
+    it('3: should uppercase first if all lowercase is given', () => {
+        const result = upperFirst('test')
+        expect(result).toBe('Test')
+    })
+    it('4: should not do anything if first is not char', () => {
         const result = upperFirst('7EST')
         expect(result).toBe('7EST')
     })
